97c6c7bbd96e3eda0eb9fba4566eec6dfa82b2df
Elden Sim <2b03.2112758.eldensim@gmail.com>
2023-06-15 17:01:55 +0800

    get status working without validation

--- src/access/services.rs
+++ src/access/services.rs
@@ -1,10 +1,52 @@
 use actix_web::{get, post, web, Responder, HttpResponse};
+use serde::{Deserialize, Serialize};
 use crate::{AppState, KMEStorageData};
 use super::models::{CreateKeyRequest};
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct Status {
+    source_KME_ID: String,
+    target_KME_ID: String,
+    master_SAE_ID: String,
+    slave_SAE_ID: String,
+    key_size: u32,
+    max_key_count: u32,
+    max_key_per_request: u32,
+    min_key_size: u32,
+    max_key_size: u32,
+    stored_key_count: u32,
+    max_SAE_ID_count: u32,
+}
+
 #[get("/api/v1/keys/{slave_SAE_ID}/status")]
-async fn get_status(data:web::Data<AppState>) -> impl Responder {
-    let storage_data = data::KMEStorageData.lock().clone();
-    
-    println!(storage_data)
+async fn get_status(data: web::Data<AppState>, path: web::Path<String>) -> impl Responder {
+    let storage_data = data.kme_storage_data.lock().unwrap().clone();
+    let key_data = data.key_data.lock().unwrap().clone();
+    let slave_SAE_ID = path.into_inner();
+    // for KME_ID in key_data.keys {
+    //     if slave_SAE_ID != KME_ID {
+    //         HttpResponse::
+    //     }
+    // }
+    let status = Status {
+        source_KME_ID: storage_data.KME_ID,
+        target_KME_ID: "???".to_string(),
+        master_SAE_ID: storage_data.SAE_ID,
+        slave_SAE_ID: slave_SAE_ID,
+        key_size: storage_data.key_size,
+        max_key_count: storage_data.max_key_count,
+        max_key_per_request: storage_data.max_key_per_request,
+        min_key_size: storage_data.min_key_size,
+        max_key_size: storage_data.max_key_size,
+        stored_key_count: storage_data.stored_key_count,
+        max_SAE_ID_count: storage_data.max_SAE_ID_count,
+    };
+    HttpResponse::Ok().json(status)
+
+}
+
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg
+        .service(get_status);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use dotenv::dotenv;
 use serde_json::Value;
 use std::fs;
+use std::collections::HashMap;
 // use std::error::Error;
 
 use actix_web::{get, web, App, HttpServer};
@@ -9,100 +10,141 @@ use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
 use std::fmt::Display;
 
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+mod access;
+use access::services;
 
 struct AppState {
-    // KME_storage_data: KMEStorageData,
-    // KME_key_data: Vec<KeyContainer>,
-    // KME_status_data: KMEStatus
-    Temp: String
+    kme_storage_data: Mutex<KMEStorageData>,
+    kme_key_data: Mutex<KeyContainer>,
+    // Temp: String
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 struct KMEStorageData {
     KME_ID: String,
     SAE_ID: String,
-    key_size: i64,
-    max_key_count: i64,
-    max_key_per_request: i64,
-    min_key_size: i32,
-    max_key_size: i32,
-    max_SAE_ID_count: i32,
     QKD_ID: String,
-    stored_key_count: i32,
-    // keys: Vec<KeyContainer>,
-}
-
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-struct KMEStatus {
-    source_KME_ID: String,
-    target_KME_ID: String,
-    master_SAE_ID: String,
-    slave_SAE_ID: String,
-    key_size: i32,
-    stored_key_count: i32,
-    max_key_count: i32,
-    max_key_per_request: i32,
-    max_key_size: i32,
-    max_SAE_ID_count: i32,
+    key_size: u32,
+    max_key_count: u32,
+    max_key_per_request: u32,
+    min_key_size: u32,
+    max_key_size: u32,
+    max_SAE_ID_count: u32,
+    stored_key_count: u32,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 struct KeyContainer {
+    keys: Vec<Key>
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct Key {
+    KME_ID: String,
     key_ID: String,
     key: String,
     vendor: String,
-    KME_ID: String,
-    extensions: Vec<KeyExtensions>,
+    extensions: Vec<KeyExtensions>
+    // extensions: HashMap<String, KeyExtensions>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Default, Debug)]
-struct KeyExtensions;
+struct KeyExtensions {
+    route_type: Option<String>,
+    max_age: Option<String>,
+    transfer_method: Option<String>,
+}
+
+
+
+// #[derive(Serialize, Deserialize, Clone, Debug)]
+// struct KMEStatus {
+//     source_KME_ID: String,
+//     target_KME_ID: String,
+//     master_SAE_ID: String,
+//     slave_SAE_ID: String,
+//     key_size: i32,
+//     stored_key_count: i32,
+//     max_key_count: i32,
+//     max_key_per_request: i32,
+//     max_key_size: i32,
+//     max_SAE_ID_count: i32,
+// }
+
+#[derive(Debug, Clone)]
+struct Config {
+    kme_id: String,
+    storage_path: String,
+    key_data_path: String,
+}
+
+impl Config {
+    fn build() -> Result<Config, &'static str> {
+        let kme_id = env::var("KME_ID").expect("KME_ID env var not set");
+        let storage_path = env::var("STORAGE_PATH").expect("STORAGE_PATH env var not set");
+        let key_data_path = env::var("KEY_DATA_PATH").expect("KEY_DATA_PATH env var not set");
+        Ok(Config {
+            kme_id,
+            storage_path,
+            key_data_path
+        })
+    }
+}
+
+fn get_KME_storage_data(storage_path: String) -> Result<KMEStorageData, Box<dyn Error>> {
+    // Open file in read-only mode.
+    let file = File::open(storage_path)?;
+    let reader = BufReader::new(file);
+    let result = serde_json::from_reader(reader)?;
+    Ok(result)
+}
+
+fn get_key_data(storage_path: String) -> Result<KeyContainer, Box<dyn Error>> {
+    let file = File::open(storage_path)?;
+    let reader = BufReader::new(file);
+    let key_data = serde_json::from_reader(reader)?;
+    Ok(key_data)
+}
+
 
 #[get("/")]
 async fn index() -> String {
     "This is a test".to_string()
 }
 
-#[derive(Debug, Clone)]
-struct Config {
-    KME_ID: String,
-    STORAGE_PATH: String,
-}
 
-impl Config {
-    fn build() -> Result<Config, &'static str> {
-        let KME_ID = env::var("KME_ID").expect("KME_ID var not set");
-        let STORAGE_PATH = env::var("STORAGE_PATH").expect("STORAGE_PATH var not set");
-        Ok(Config {
-            KME_ID,
-            STORAGE_PATH
-        })
-    }
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     let config = Config::build();
-    println!("{:?}", config.clone().unwrap());
-    // let storage_data = KMEStorageData::build(config.unwrap().STORAGE_PATH);
-    // let key_data = Vec<KeyContainer> {};
-    // let status_data = KMEStatus {};
+    let (kme_id, storage_path, key_data_path) = (config.clone().unwrap().kme_id, config.clone().unwrap().storage_path, config.clone().unwrap().key_data_path);
+
+    // println!("{}", kme_id);
+    // println!("{}", storage_path);
+    let data = get_KME_storage_data(storage_path);
+    // println!("{:?}", data);
+    let key_data = get_key_data(key_data_path);
+    // println!("{:?}", key_data);
+
+
     let app_data = web::Data::new(AppState {
-        Temp: "Not functional".to_string(),
-        // KME_storage_data: storage_data.unwrap(),
-        // KME_key_data: key_data,
-        // KME_status_data: status_data
+        kme_storage_data: data.unwrap().clone().into(),
+        kme_key_data: key_data.unwrap().clone().into(),
     });
 
     HttpServer::new(move || {
         App::new()
             .app_data(app_data.clone())
             .service(index)
-            // .configure(service::config)
+            .configure(services::config)
     })
-    .bind(("127.0.0.1", 5000))?
+    .bind(("127.0.0.1", 5001))?
     .run()
     .await
 }
