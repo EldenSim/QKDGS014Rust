eab3071e3894f589ecbbcc16e99212bbaed5e9b4
Elden <elden.str@gmail.com>
2023-06-27 16:55:14 +0800

    Decode done, encode not done

--- src/access/services.rs
+++ src/access/services.rs
@@ -3,6 +3,7 @@ use actix_web::{get, post, web, HttpRequest, HttpResponse, Responder};
 use super::models::CreateKeyRequest;
 use crate::{AppState, KMEStorageData, Key, KeyContainer};
 use base64::{engine::general_purpose, Engine as _};
+use num::BigUint;
 use serde::{Deserialize, Serialize};
 use std::str;
 
@@ -25,6 +26,9 @@ struct Status {
 struct GeneralError {
     message: String,
 }
+struct GeneralWarning {
+    message: String,
+}
 
 #[get("/api/v1/keys/{slave_SAE_ID}/status")]
 async fn get_status(data: web::Data<AppState>, path: web::Path<String>) -> impl Responder {
@@ -38,10 +42,9 @@ async fn get_status(data: web::Data<AppState>, path: web::Path<String>) -> impl 
         return HttpResponse::BadRequest().json(error);
     };
     let mut matched_keys: Vec<Key> = Vec::new();
-    let key_data_iter: std::slice::Iter<'_, Key> = key_data.keys.iter();
-    for key in key_data_iter {
+    for key in key_data.keys.iter() {
         if key.KME_ID[3..] == slave_SAE_ID[3..] {
-            matched_keys.insert(0, key.clone())
+            matched_keys.push(key.clone())
         }
     }
     let stored_key_count: usize = if matched_keys.is_empty() {
@@ -99,23 +102,59 @@ async fn get_keys_get(
         Some(x) => matched_keys.splice(0..x as usize, []).collect(),
         _ => matched_keys,
     };
-    // let mut matched_keys = match size {
-    //     Some(x) => {
-    //         for key in &mut matched_keys {
-    //             let key_string: String = key.key;
-    //             let key_string_decode = decode(key_string);
-    //             let key_string_truc = &key_string_decode[0..x as usize];
-    //             let key_truc_encode = encode(key_string_truc.to_string());
-    //             // matched_keys.insert()
-    //         }
-    //         matched_keys
-    //         // matched_keys.iter_mut().map(|Key {
-    //         //     key_ID,
-    //         //     key: decode(key)
-    //         // }|)
-    //     }
-    //     _ => matched_keys,
-    // };
+    let mut matched_keys = match size {
+        // general_purpose::STANDARD_NO_PAD.decode(key).unwrap()
+        //  decoded_key_vec.iter_mut().map(|x| (*x - 48)).collect()
+        Some(x) => {
+            for key in &mut matched_keys {
+                let key_str = &key.key;
+                println!("{}", key_str);
+                let decoded_bytes_res = general_purpose::STANDARD.decode(key_str);
+                let decoded_bytes = match decoded_bytes_res {
+                    Ok(res) => res,
+                    Err(e) => {
+                        println!("Failed Decoding Key: {}", e);
+                        break;
+                    }
+                };
+                println!("{:?}", decoded_bytes);
+                let decoded_bytes_scaled: Vec<_> = decoded_bytes.iter().map(|x| *x - 48).collect();
+                let decoded_str: String = decoded_bytes_scaled
+                    .iter()
+                    .map(|&num| num.to_string())
+                    .collect();
+                let decoded_key: BigUint = decoded_str.parse().unwrap();
+                let decoded_key_str = format!("{:b}", decoded_key);
+                if decoded_key.bits() < x {
+                    // let warning: GeneralWarning = GeneralWarning {
+                    //     message: "Key"
+                    // }
+                    continue;
+                }
+                let truc_key_bin = &decoded_key_str[0..x as usize];
+                // Encode the key back
+                let truc_key_bytes = truc_key_bin.as_bytes();
+                println!("{:?}", truc_key_bytes);
+                let truc_key_dec: BigUint = BigUint::from_radix_be(truc_key_bytes, 10).unwrap();
+
+                // *key.key = truc_key
+                println!("{}", truc_key_bin);
+                println!("{}", truc_key_dec);
+                // println!("{}", decoded_str);
+                // println!("{}", decoded_key);
+                // println!("{}", decoded_key.bits());
+                // println!("{:b}", decoded_key);
+                println!()
+            }
+
+            matched_keys
+            // matched_keys.iter_mut().map(|Key {
+            //     key_ID,
+            //     key: decode(key)
+            // }|)
+        }
+        _ => matched_keys,
+    };
 
     HttpResponse::Ok().json(matched_keys)
 }
